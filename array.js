4b4f00aff7a589e5ee4844b70ad8ca38a98bdd92
mouna <mouna.khila@aleriaconsulting.eu>
2023-01-04 20:53:20 +0100

    basics

--- array.js
+++ array.js
@@ -1,1 +1,59 @@
-
+//1
+var myArray = ["dog", "cat", "fox", "monkey"];
+//2
+myArray[0];
+myArray[1];
+myArray[myArray.length - 1];
+//3
+var arr = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  "The",
+  "quick",
+  "brown",
+  "fox",
+  "jumped",
+  "over",
+  "the",
+  "lazy",
+  "dog",
+  true,
+  false,
+  true,
+];
+//
+function emptyArray() {
+  return [];
+}
+//5
+function numbersArray() {
+  var arr = [1, 2, 3, 4, 5];
+  return arr;
+}
+//
+function numbersArray() {
+  var arr = [];
+  for (i = 1; i <= 5; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
+//6
+function booleansArray() {
+  var arr = [true, true, true];
+  return arr;
+}
+//7
+function stringsArray() {
+  var arr = ["Mouna", "Khila"];
+  return arr;
+}
+//8
+function arrayLength(array) {
+  return arr.length;
+}
