593cf60c10b9549375829587c1f64ce0f651060f
mouna <mouna.khila@aleriaconsulting.eu>
2023-01-10 20:27:44 +0100

    advanced iteration

--- iterationWhileFor.js
+++ iterationWhileFor.js
@@ -67,7 +67,7 @@ function average(array) {
   for (var i = 0; i < array.length; i++) {
     avg += array[i];
   }
-  return avg / array.length;
+  return (avg / array.length).toFixed(0) * 1;
 }
 //10
 function square(array) {
@@ -80,4 +80,84 @@ function square(array) {
 //11
 function isArray(array) {
   return Array.isArray(array);
+}
+//More Practice
+//1
+function min(array) {
+  var minValue = array[0];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] < minValue) {
+      minValue = array[i];
+    }
+  }
+  return minValue;
+}
+//2
+function max(array) {
+  var maxValue = array[0];
+  for (var i = 1; i < array.length; i++) {
+    if (maxValue < array[i]) {
+      maxValue = array[i];
+    }
+  }
+  return maxValue;
+}
+//3
+function shortestString(array) {
+  let shortestLength = array[0].length;
+  for (let i = 1; i < array.length; i++) {
+    shortestLength = Math.min(shortestLength, array[i].length);
+  }
+  return shortestLength;
+}
+//4
+function longestString(array) {
+  var longestLength = array[0].length;
+  for (var i = 0; i < array.length; i++) {
+    longestLength = Math.max(longestLength, array[i].length);
+  }
+  return longestLength;
+}
+//5
+function shortestLongest(array) {
+  let shortest = array[0];
+  let longest = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i].length < shortest.length) {
+      shortest = array[i];
+    } else if (array[i].length > longest.length) {
+      longest = array[i];
+    }
+  }
+
+  return [shortest, longest];
+}
+//6
+function minMax(array) {
+  var min = array[0];
+  var max = array[0];
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    } else if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return [min, max];
+}
+//7
+function multiplyBy(array, number) {
+  return array.map((element) => element * number);
+}
+//or
+function multiplyBy(array, number) {
+  var newarray = [];
+  array.map((element) => newarray.push(element * number));
+  return newarray;
+}
+//8
+function multiplyByIndex(array) {
+  var newArray = [];
+  array.map((element, index) => newArray.push(element * index));
+  return newArray;
 }
